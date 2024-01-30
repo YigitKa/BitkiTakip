f2a8d3238370a3aaec7d3fe8cec7df139d1ba5e1
Yiğit KATIRCIOĞLU <90yigit@gmail.com>
2024-01-30 03:46:02 +0300

    Web App UI teması eklendi
    Web App UI teması eklendi

---
+++ Application/Bitky_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Bitky_WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
