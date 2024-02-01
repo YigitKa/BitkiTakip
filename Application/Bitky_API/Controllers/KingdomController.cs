29b6123228a5860269265adb6330efc1250f81cc
Yiğit KATIRCIOĞLU <90yigit@gmail.com>
2024-02-01 22:22:25 +0300

    Kingdom Controllerı güncellendi
    Kingdom controller fonksiyonları eklendi

--- Application/Bitky_API/Controllers/KingdomController.cs
+++ Application/Bitky_API/Controllers/KingdomController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bitky_API.DTOs.KingdomDTOs;
+using Bitky_API.Repositories.KingdomRepository;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +12,52 @@ namespace Bitky_API.Controllers
 {
     public class KingdomController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly IKingdomRepository _kingdomRepository;
+        public KingdomController(IKingdomRepository KingdomRepository)
         {
-            return View();
+            _kingdomRepository = KingdomRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> KingdomList()
+        {
+            var values = await _kingdomRepository.GetAllKingdomsAsync();
+            return Ok(values);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteKingdom([FromBody] DeleteKingdomDTO deleteKingdomDTO)
+        {
+            bool result = _kingdomRepository.DeleteKingdom(deleteKingdomDTO);
+            if (!result)
+            {
+                return Problem("Alem silinirken hata oluştu.");
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateKingdom([FromBody] CreateKingdomDTO createKingdomDTO)
+        {
+            bool result = _kingdomRepository.CreateKingdom(createKingdomDTO);
+
+            if (!result)
+            {
+                return Problem("Alem eklenirken hata oluştu.");
+            }
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateKingdom([FromBody] UpdateKingdomDTO updateKingdomDTO)
+        {
+            bool result = _kingdomRepository.UpdateKingdom(updateKingdomDTO);
+
+            if (!result)
+            {
+                return Problem("Alem eklenirken güncellenirken oluştu.");
+            }
+            return Ok();
         }
     }
 }
