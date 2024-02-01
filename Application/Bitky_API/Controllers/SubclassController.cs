e2fbc554d370699dbfe4e1d24cbad72e247b0959
Yiğit KATIRCIOĞLU <90yigit@gmail.com>
2024-02-01 22:29:31 +0300

    Subclass controllerı güncellendi
    subclass controller fonksiyonları eklendi

--- Application/Bitky_API/Controllers/SubclassController.cs
+++ Application/Bitky_API/Controllers/SubclassController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bitky_API.DTOs.SubclassDTOs;
+using Bitky_API.Repositories.SubclassRepository;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +12,52 @@ namespace Bitky_API.Controllers
 {
     public class SubclassController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly ISubclassRepository _subclassRepository;
+        public SubclassController(ISubclassRepository subclassRepository)
         {
-            return View();
+            _subclassRepository = subclassRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SubclassList()
+        {
+            var values = await _subclassRepository.GetAllSubclassesAsync();
+            return Ok(values);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSubclass([FromBody] DeleteSubclassDTO deleteSubclassDTO)
+        {
+            bool result = _subclassRepository.DeleteSubclass(deleteSubclassDTO);
+            if (!result)
+            {
+                return Problem("Alt cins silinirken hata oluştu.");
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSubclass([FromBody] CreateSubclassDTO createSubclassDTO)
+        {
+            bool result = _subclassRepository.CreateSubclass(createSubclassDTO);
+
+            if (!result)
+            {
+                return Problem("Alt cins eklenirken hata oluştu.");
+            }
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateSubclass([FromBody] UpdateSubclassDTO updateSubclassDTO)
+        {
+            bool result = _subclassRepository.UpdateSubclass(updateSubclassDTO);
+
+            if (!result)
+            {
+                return Problem("Alt cins eklenirken güncellenirken oluştu.");
+            }
+            return Ok();
         }
     }
 }
