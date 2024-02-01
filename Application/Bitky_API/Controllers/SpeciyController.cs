4de1ee2b61fe9f7911141554c074fd74fa13ce9d
Yiğit KATIRCIOĞLU <90yigit@gmail.com>
2024-02-01 22:27:22 +0300

    Speciy Controllerı güncellendi
    Speciy controller fonksiyonları eklendi

--- Application/Bitky_API/Controllers/SpeciyController.cs
+++ Application/Bitky_API/Controllers/SpeciyController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bitky_API.DTOs.SpeciyDTOs;
+using Bitky_API.Repositories.SpeciyRepository;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +12,52 @@ namespace Bitky_API.Controllers
 {
     public class SpeciyController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly ISpeciyRepository _speciyRepository;
+        public SpeciyController(ISpeciyRepository speciyRepository)
         {
-            return View();
+            _speciyRepository = speciyRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SpeciyList()
+        {
+            var values = await _speciyRepository.GetAllSpeciesAsync();
+            return Ok(values);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSpeciy([FromBody] DeleteSpeciyDTO deleteSpeciyDTO)
+        {
+            bool result = _speciyRepository.DeleteSpeciy(deleteSpeciyDTO);
+            if (!result)
+            {
+                return Problem("Tür silinirken hata oluştu.");
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSpeciy([FromBody] CreateSpeciyDTO createSpeciyDTO)
+        {
+            bool result = _speciyRepository.CreateSpeciy(createSpeciyDTO);
+
+            if (!result)
+            {
+                return Problem("Tür eklenirken hata oluştu.");
+            }
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateSpeciy([FromBody] UpdateSpeciyDTO updateSpeciyDTO)
+        {
+            bool result = _speciyRepository.UpdateSpeciy(updateSpeciyDTO);
+
+            if (!result)
+            {
+                return Problem("Tür eklenirken güncellenirken oluştu.");
+            }
+            return Ok();
         }
     }
 }
